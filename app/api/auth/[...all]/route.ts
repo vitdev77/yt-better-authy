6ad131e39b0d0d61177e75f772ce421df33057e8
vitdev77 <vit.dev.77@gmail.com>
2025-11-16 17:06:05 +0300

    16112025_002_commit

---
+++ app/api/auth/[...all]/route.ts
@@ -0,0 +1,10 @@
+import { auth } from '@/lib/auth'; // path to your auth file
+import { toNextJsHandler } from 'better-auth/next-js';
+
+export const { POST, GET } = toNextJsHandler(auth);
+
+// this route.ts file lives in /api/auth/[...all]
+
+// /api/auth/sign-in
+// /api/auth/sign-up
+// /api/auth/get-session
